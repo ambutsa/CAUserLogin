d7e65d6cef8e7b2124c18b11fedee08d26ed1f55
ambutsa <145941214+ambutsa@users.noreply.github.com>
2024-10-28 20:48:45 -0400

    2.2

--- src/main/java/use_case/signup/SignupInteractor.java
+++ src/main/java/use_case/signup/SignupInteractor.java
@@ -31,7 +31,6 @@ public class SignupInteractor implements SignupInputBoundary {
             final User user = userFactory.create(signupInputData.getUsername(), signupInputData.getPassword());
             userDataAccessObject.save(user);
 
-
             final SignupOutputData signupOutputData = new SignupOutputData(user.getName(), false);
             userPresenter.prepareSuccessView(signupOutputData);
         }
